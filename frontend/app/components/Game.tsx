480b49701d3a8c9f8617ca9d5fe718af13d5b9ae
Dorian GARCIA <93933273+Dorian-Garcia-pro@users.noreply.github.com>
2024-07-03 03:21:43 +0200

    structuring & icons & checks

--- frontend/app/components/Game.tsx
+++ frontend/app/components/Game.tsx
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import Image from "next/image";
 import axios from "axios";
 import { IPlayer } from "../../../backend/models/Player";
+import "../styles/games.scss";
+import rock from "../assets/icons8-fist-96.png";
+import paper from "../assets/icons8-hand-96.png";
+import scissors from "../assets/icons8-hand-scissors-96.png";
 
 const Game = () => {
-  const [username, setUsername] = useState("");
-  const [result, setResult] = useState("");
-  const [playerInfos, setPlayerInfos] = useState<IPlayer>();
+  const [username, setUsername] = useState<string>("");
+  const [result, setResult] = useState<string>("");
+  const [computerMove, setComputerMove] = useState<string>("");
+  const [playerInfos, setPlayerInfos] = useState<IPlayer | undefined>();
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handlePlay = async (move: string) => {
     try {
-      // Check if the username is at least 3 characters long
-      if (username.length < 3) {
-        alert("Username must be at least 3 characters long");
-        return;
-      }
+  
       // Send username and move to the backend
       const response = await axios.post("http://localhost:5000/api/game/play", {
         username,
@@ -23,6 +26,8 @@ const Game = () => {
       setResult(
         `You ${response.data.result}. Computer chose ${response.data.computerMove}.`
       );
+      setResult(response.data.result);
+      setComputerMove(response.data.computerMove);
       // Update the player's stats
       setPlayerInfos(response.data as IPlayer);
     } catch (error) {
@@ -37,47 +42,110 @@ const Game = () => {
         { username }
       );
       setPlayerInfos({
+        username: "", 
         score: 0,
         streak: 0,
         bestStreak: 0,
         gamePlayed: 0,
         wins: 0,
       } as IPlayer);
-      setResult("");
+      setResult("restart");
+      setUsername("");
+      if (inputRef.current) {
+        (inputRef.current as any).value = ""; // Empty the input
+      }
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleStart = (uname: string) => {
+    // Check if the username is at least 3 characters long
+    if (uname.length < 3 || uname.length > 16) {
+      console.log(uname.length)
+      alert("Username must be between 3 and 16 characters long");
+      return;
+    }
+    if (inputRef.current) {
+      setUsername((inputRef.current as HTMLInputElement).value);
+      setPlayerInfos({
+        username: (inputRef.current as HTMLInputElement).value,
+        score: 0,
+        streak: 0,
+        bestStreak: 0,
+        gamePlayed: 0,
+        wins: 0,
+      })
+    }
+  };
+
+  const confirmReset = () => {
+    if (window.confirm("Are you sure you want to reset the game? Your progress will be lost.")) {
+      handleReset();
+    }
+  };
+
+
   return (
-    <div>
+    <div className="gameMain">
+      {!username && (
+        <>
       <input
         type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        ref={inputRef}
         placeholder="Enter your username"
         required
-      />
-      <br></br>
-      <button onClick={() => handlePlay("rock")}>Rock</button>
-      <button onClick={() => handlePlay("paper")}>Paper</button>
-      <button onClick={() => handlePlay("scissors")}>Scissors</button>
-      {playerInfos && (
+        className="inputUsername"
+      />      
+      <button onClick={(e) =>  handleStart(inputRef.current?.value as string) }>Valider</button>
+      </>
+      )}
+
+      {username && (
         <>
-          <p>{result}</p>
-          <p>Your score: {playerInfos.score}</p>
-          <p>Streak: {playerInfos.streak}</p>
-          <p>Best streak : {playerInfos.bestStreak}</p>
-          <p>gamePlayed : {playerInfos.gamePlayed}</p>
-          <p>Wins : {playerInfos.wins}</p>
-          <p>
-            Win percentage :
-            {playerInfos.wins && playerInfos.gamePlayed
-              ? ((playerInfos.wins / playerInfos.gamePlayed) * 100).toFixed(2)
-              : 0}
-            %
-          </p>
-          <button onClick={() => handleReset()}>Reset</button>
+              <div className="btnMoves">
+        <button onClick={() => handlePlay("rock")}>
+          <Image src={rock} alt="rock button" />
+        </button>
+        <button onClick={() => handlePlay("paper")}>
+          <Image src={paper} alt="paper button" />
+        </button>
+        <button onClick={() => handlePlay("scissors")}>
+          <Image src={scissors} alt="scissors button" />
+        </button>
+      </div>
+         {result !== "restart" && (
+          <div className="resultDisplay">
+            <p data-result={result}>You {result}</p>
+            <p>Computer chose {computerMove}</p>
+          </div>
+         )}
+          <div className="stats">
+            <div>
+              <p>Score : {playerInfos?.score}</p>
+              <span>|</span>
+              <p>
+                Streak : {playerInfos?.streak} (Best : {playerInfos?.bestStreak}
+                )
+              </p>
+            </div>
+
+            <div>
+              <p>
+                Wins : {playerInfos?.wins}/{playerInfos?.gamePlayed} (
+                {playerInfos?.wins && playerInfos?.gamePlayed
+                  ? (
+                      (playerInfos?.wins / playerInfos?.gamePlayed) *
+                      100
+                    ).toFixed(2)
+                  : 0}
+                %)
+              </p>
+            </div>
+          </div>
+          <button className="resetButton" onClick={() => confirmReset()}>
+            Reset
+          </button>
         </>
       )}
     </div>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import Game from "./components/Game";
 import Leaderboard from "./components/Leaderboard";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Paper-Scissors-Rocks Game</h1>
+{/*       <h1>Paper-Scissors-Rocks Game</h1> */}
+<div></div>
       <Game />
       <Leaderboard />
     </main>
